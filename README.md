4d7d27c5b4f7516f48a490f7fb349509e7c68ecf
PeaceM4ker91 <peace.m4ker@hotmail.com>
2023-05-05 13:38:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Almacen
+ tarea unab
---
+++ src/main/java/cl/unab/vinna/main/Almacen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.unab.vinna.main;
+
+/**
+ *
+ * @author PeaceM4ker
+ */
+public class Almacen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
